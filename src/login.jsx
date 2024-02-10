d580dea3265d5541897f485ed29ab0b19f7ce088
Dulmini Senewirathna <senewirathnapabodha@gmail.com>
2024-02-10 10:16:56 +0530

    React Commit

---
+++ src/login.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+
+
+export const Login = (props) =>
+{
+    const [email,setEmail] = useState('');
+    const [pass,setPass] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return(
+        <div className="auth-form-container">
+        <form action="" className="login-form" onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" placeholder="abc@gmail.com" />
+
+            <label htmlFor="password">Password</label>
+            <input value={pass}onChange={(e) => setPass(e.target.value)} type="password" id="password" placeholder="*********" />
+
+            <button type="submit">Login</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+} 
